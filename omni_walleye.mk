b985164ba739b6a8a8610c137c123cf95c6cc828
Alexander Diewald <diewald@fortiss.org>
2018-08-26 11:52:54 +0200

    walleye: Omnify.
    * Add omni dependencies to the wahoo board & omni makefile.
    * Rename relevant paths from device/google/muskie to
      device/google/walleye since the repo is named "walleye"
      instead of the dead "muskie" device. Also eases provisioning
      of this device-specific repo.
    
    Signed-off-by: Alexander Diewald <diewald@fortiss.org>

---
+++ omni_walleye.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2017 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit common Omni APNs.
+$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit device configuration
+$(call inherit-product, device/google/walleye/aosp_walleye.mk)
+
+# Include walleyes board config.
+include device/google/wahoo/omni_wahoo.mk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := omni_walleye
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 2
+TARGET_MANUFACTURER := Google
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=walleye \
+    BUILD_FINGERPRINT=google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+
+$(call inherit-product-if-exists, vendor/google/walleye/walleye-vendor.mk)
